8b294ce394403b2e9503950c9e502e3c2b96db52
Farjord <cpollicott@hotmail.com>
2025-12-09 22:03:36 +0000

    Pre Extraction Rewards Revamp Pt 2

--- src/data/aiData.js
+++ src/data/aiData.js
@@ -103,7 +103,7 @@ const aiPersonalities = [
       'Scout Drone',
       'Standard Fighter',
       'Interceptor',
-      'Scanner Drone'
+      'Scanner'
     ],
     shipDeployment: {
       strategy: 'defensive',
